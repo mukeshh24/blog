dc1b8f4f04e7f3bcab66b7d9e1b688ae18cfdae7
mukeshh24 <s.mukesh1403@gmail.com>
2026-05-20 13:04:00 +0530

    fix: handle cors preflight requests

--- server/app.js
+++ server/app.js
@@ -13,8 +13,12 @@ app.use(
   cors({
     origin: ["http://localhost:5173", "https://blog-g9rx.vercel.app"],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   }),
 );
+
+app.options("*", cors());
+
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
