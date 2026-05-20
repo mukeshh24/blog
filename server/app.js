d809d6b347ae3dad212faa4ed71115213bda3e79
mukeshh24 <s.mukesh1403@gmail.com>
2026-05-20 12:54:07 +0530

    fix: update cors for vercel frontend deployment

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://blog-rho-nine-41.vercel.app"],
     credentials: true,
   }),
 );
